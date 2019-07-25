d450108ca8c3c3ce7d5dcf06c8b17a60592db1f9
pipelineascodecourse <committer@example.com>
2019-07-25 18:53:11 +0800

    initial commit

--- vars/executeInScriptWithClasses.groovy
+++ vars/executeInScriptWithClasses.groovy
@@ -1,10 +1,14 @@
 import com.acceleratedskillup.*;
 
-def call(body) {
+def call(body, script) {
 
 	script{
       body()
 	}
 	
-	def calculator = new Calculator()
+	def myCar = new Car(script)
+	myCar.numberOfDoors = 5
+	myCar.brand = "123"
+	myCar.model = "abc"
+	myCar.printOutCar()
 }
